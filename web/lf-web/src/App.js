1fb8db4e1729ee6b11db67c8e9a3565c50eaf3d8
patrickmziet <patrickzietkiewicz@gmail.com>
2023-11-02 11:25:03 +0000

    added AuthProvider as parent to other components

--- web/lf-web/src/App.js
+++ web/lf-web/src/App.js
@@ -4,22 +4,23 @@ import Notes from './components/Notes';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
 import Register from './components/Register';
-// import PrivateRoute from './components/PrivateRoute';
+import { AuthProvider } from './contexts/AuthContext';
+//import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Router>
+      <AuthProvider>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/notes" element={<Notes />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
         {/* Add more routes as needed */}
       </Routes>
+      </AuthProvider>
     </Router>
   );
 }
 
 export default App;
-
-// <PrivateRoute path="/notes" element={<Notes /> } />
