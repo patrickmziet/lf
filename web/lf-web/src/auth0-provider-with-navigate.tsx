06e4553a25fd3df3f40203c0dfe16ddaef3bd5bc
patrickmziet <patrickzietkiewicz@gmail.com>
2023-11-28 18:31:17 +0000

    another version which fails

--- web/lf-web/src/auth0-provider-with-navigate.tsx
+++ web/lf-web/src/auth0-provider-with-navigate.tsx
@@ -1,40 +1,48 @@
 import { Auth0Provider, AppState, useAuth0 } from "@auth0/auth0-react";
-import React, { PropsWithChildren } from "react";
-import { useNavigate } from "react-router-dom";
-import { createUserIfNotExist } from "./services/message.service";
+import React, { useEffect, PropsWithChildren } from "react"; // React imports
+import { useNavigate } from "react-router-dom"; // React Router hook
+import { createUserIfNotExist } from "./services/message.service"; // Your custom service
 
 interface Auth0ProviderWithNavigateProps {
-  children: React.ReactNode;
+  children: React.ReactNode; // Prop type
 }
+
+const Auth0Wrapper = ({ children }: { children: React.ReactNode }) => { // Component
+  const navigate = useNavigate(); // Hook
+  const auth0 = useAuth0(); // Hook
+
+  useEffect(() => { // Hook
+    const onRedirectCallback = async (appState?: AppState) => { // Function
+      navigate(appState?.returnTo || window.location.pathname); // Navigation logic
+      console.log("appState", appState?.returnTo); 
+
+      try {
+        const token = await auth0.getAccessTokenSilently(); // Auth0 method
+        const response = await createUserIfNotExist(token); // Custom service call
+        console.log(response);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message); // Error handling
+        }
+      }
+    };
+
+    onRedirectCallback(); // Invoke the callback
+  }, [auth0, navigate]); // Dependency array for useEffect
+
+  return <>{children}</>; // Render children
+};
 
 export const Auth0ProviderWithNavigate = ({
   children,
 }: PropsWithChildren<Auth0ProviderWithNavigateProps>): JSX.Element | null => {
-  const navigate = useNavigate();
-  const auth0 = useAuth0();
-
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
-  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
-
-  const onRedirectCallback = async (appState?: AppState) => {
-      navigate(appState?.returnTo || window.location.pathname);
-      console.log("appState", appState?.returnTo);
-      
-      try {
-        const token = await auth0.getAccessTokenSilently();
-        const response = await createUserIfNotExist(token);
-        console.log(response);
-      } catch (error) {
-        if (error instanceof Error) {
-          console.log(error.message);
-        }
-      }
-  };
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN; // Environment variable
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID; // Environment variable
+  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL; // Environment variable
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE; // Environment variable
 
   if (!(domain && clientId && redirectUri && audience)) {
-    return null;
+    return null; // Guard clause for missing env variables
   }
 
   return (
@@ -45,9 +53,10 @@ export const Auth0ProviderWithNavigate = ({
         audience: audience,
         redirect_uri: redirectUri,
       }}
-      onRedirectCallback={onRedirectCallback}
     >
-      {children}
+      <Auth0Wrapper>
+        {children} 
+      </Auth0Wrapper>
     </Auth0Provider>
   );
 };
