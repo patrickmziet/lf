e2c605289f5f804837332286ddb56f7e81cade3e
patrickmziet <patrickzietkiewicz@gmail.com>
2024-01-19 16:35:47 +0000

    removed comments

--- backend/learnfast/core/urls.py
+++ backend/learnfast/core/urls.py
@@ -1,16 +1,3 @@
-""" from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import NoteViewSet
-
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
-router.register(r'notes', NoteViewSet, basename='note')
-
-# The API URLs are now determined automatically by the router.
-urlpatterns = [
-    path('', include(router.urls)),
-]
- """
 from django.urls import path
 
 from .views import (
