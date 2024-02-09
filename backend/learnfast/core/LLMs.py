d283713f5afa3e5dab5a5b07b3c732004ba7399d
patrickmziet <patrickzietkiewicz@gmail.com>
2024-02-09 17:10:56 +0000

    changed generate flashcards func

--- backend/learnfast/core/LLMs.py
+++ backend/learnfast/core/LLMs.py
@@ -1,14 +1,28 @@
 # OpenAI/AI API imports
 from openai import OpenAI
 import os
+# LLM management package
+from pana.models import ModelFactory
+from pana.lparse import parse_json_string
 # Local imports
 from .models import Flashcard
 
 
-def generate_flashcards(msg_chn, topic_id):
+def generate_flashcards(msg_chn, topic_id):   
+    model = ModelFactory.get_model("gpt-3.5-turbo-0125", api_key=os.environ.get("OPENAI_API_KEY"))
+    response = model.make_call(msg_chn)
+    parsed_flashcards = parse_json_string(response.choices[0].message.content)
+    for card_number, card in parsed_flashcards.items():
+        Flashcard.objects.create(
+            topic_id=topic_id,
+            question=card['Question'],
+            answer=card['Answer'],
+        )
+    # YOU ARE HERE, NEED TO ADJUST HOW MSG_CHN IS MADE, DO JSON VERSION
+    
     # Generate initial batch of flashcards
     # This is a very naive implementation
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+"""     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -22,7 +36,8 @@ def generate_flashcards(msg_chn, topic_id):
             question=card['Question'],
             answer=card['Answer'],
         )
-
+ """
+ 
 def parse_flashcards(text):
     flashcards = {}
     # Split text into lines
