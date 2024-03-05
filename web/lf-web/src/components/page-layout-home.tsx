9b14d58688effae185978bab6eeaf5befa643a6d
patrickmziet <patrickzietkiewicz@gmail.com>
2024-03-05 15:06:05 +0000

    page layout with footer for home screen

---
+++ web/lf-web/src/components/page-layout-home.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { NavBar } from "./navigation/desktop/nav-bar";
+import { MobileNavBar } from "./navigation/mobile/mobile-nav-bar";
+import { PageFooter } from "./page-footer";
+
+interface Props {
+    children: JSX.Element;
+}
+
+export const PageLayoutHome: React.FC<Props> = ({ children }) => {
+    const faviconUrl = process.env.PUBLIC_URL + '/favicon.ico';
+    return (
+        <div className="page-layout">
+            <Helmet>
+                <title>LearnFast</title>
+                <link rel="icon" href={faviconUrl} />
+            </Helmet>
+            <NavBar />
+            <MobileNavBar />
+            <div className="page-layout__content">{children}</div>
+            <PageFooter />
+        </div>
+    );
+};
