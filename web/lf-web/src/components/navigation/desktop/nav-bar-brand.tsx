46583ed68d85eeba955b4a9cca3620bd2e64e6e6
patrickmziet <patrickzietkiewicz@gmail.com>
2024-04-03 07:05:02 +0100

    removed link once logged in

--- web/lf-web/src/components/navigation/desktop/nav-bar-brand.tsx
+++ web/lf-web/src/components/navigation/desktop/nav-bar-brand.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Logo } from "../../logos/logo";
 
 export const NavBarBrand: React.FC = () => {
+  const { isAuthenticated } = useAuth0();
 
   return (
     <div className="nav-bar__brand">
-      <NavLink to="/">
+      {isAuthenticated ? (
         <p className="nav-bar__name">
           LearnFast
         </p>
-      </NavLink>
+      ) : (
+        <NavLink to="/">
+          <p className="nav-bar__name">
+            LearnFast
+          </p>
+        </NavLink >
+      )}
     </div>
   );
 };
